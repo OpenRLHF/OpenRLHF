db709650095ef77b8170410c3b4a997cc4cb43a6
hijkzzz <janhu9527@gmail.com>
2023-08-10 01:11:28 +0000

    add datasets

---
+++ openllama2/datasets/utils.py
@@ -0,0 +1,18 @@
+import torch.distributed as dist
+import torch
+import torch.nn.functional as F
+
+
+def zero_pad_sequences(sequences, side: str = 'left', value=0):
+    assert side in ('left', 'right')
+    max_len = max(seq.size(-1) for seq in sequences)
+    padded_sequences = []
+    for seq in sequences:
+        pad_len = max_len - seq.size(-1)
+        padding = (pad_len, 0) if side == 'left' else (0, pad_len)
+        padded_sequences.append(F.pad(seq, padding, value=value))
+    return torch.stack(padded_sequences, dim=0)
+
+
+def exist_and_not_none(d, key):
+    return key in d and d[key] is not None
