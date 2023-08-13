327806f53ff1b67ed51458c3465af9ee924dce31
hijkzzz <janhu9527@gmail.com>
2023-08-13 09:01:56 +0000

    fix train_ppo

--- examples/scripts/train_ppo_llama.sh
+++ examples/scripts/train_ppo_llama.sh
@@ -11,14 +11,14 @@ deepspeed ../train_ppo.py \
     --accumulated_gradient 16 \
     --experience_batch_size 1 \
     --update_timesteps 128 \
-    --max_epochs 1\
+    --max_epochs 1 \
     --prompt_max_len 1024 \
     --generate_max_len 1024 \
-    --zero_stage 3 \
-    --bf16\
+    --zero_stage 2 \
+    --bf16 \
     --actor_learning_rate 5e-7\
     --critic_learning_rate 9e-6\
-    --inference_tp_size 4 \
+    --inference_tp_size 1 \
     --init_kl_coef 0.01\
     --prompt_data "Dahoas/full-hh-rlhf,tasksource/oasst1_pairwise_rlhf_reward" \
     --prompt_data_probs "0.8,0.2" \
