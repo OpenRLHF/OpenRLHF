5c8be4480c35c368bb2be4daf07222fe0c29022a
hijkzzz <janhu9527@gmail.com>
2023-08-13 13:55:42 +0000

    fix args

--- examples/scripts/train_ppo_llama.sh
+++ examples/scripts/train_ppo_llama.sh
@@ -3,7 +3,7 @@ export PATH=$HOME/.local/bin/:$PATH
 
 deepspeed ../train_ppo.py \
     --pretrain "meta-llama/Llama-2-7b-hf" \
-    --critic_pretrain "$HOME/scratch/data/llama_hf/7B" \
+    --critic_pretrain "meta-llama/Llama-2-7b-hf" \
     --reward_model_path "./ckpt/7b_llama/rm_model.pt" \
     --sft_model_path "./ckpt/7b_llama/sft_model.pt" \
     --save_path './ckpt/7b_llama' \
