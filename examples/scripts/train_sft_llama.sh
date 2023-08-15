31739abf37ad00f8b8d8e197656b8a24570b2b94
hijkzzz <janhu9527@gmail.com>
2023-08-15 00:30:34 +0000

    fix prompt data name

--- examples/scripts/train_sft_llama.sh
+++ examples/scripts/train_sft_llama.sh
@@ -5,6 +5,7 @@ export CUDA_LAUNCH_BLOCKING=1
 deepspeed ../train_sft.py \
     --max_len 2048 \
     --dataset 'yahma/alpaca-cleaned' \
+    --dataset_probs '1.0' \
     --train_batch_size 128 \
     --micro_train_batch_size 1 \
     --pretrain "meta-llama/Llama-2-7b-hf" \
