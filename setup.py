bd0b2b6fba9fcdbd3d60610964282096c17569e7
Qing Wang <kingchin1218@126.com>
2023-09-21 17:45:25 +0800

    Add uploading nightly package to pypi repo in CI/CD. (#109)
    Signed-off-by: Qing Wang <kingchin1218@gmail.com>

--- setup.py
+++ setup.py
@@ -1,24 +1,41 @@
+from datetime import datetime
 from setuptools import find_packages, setup
 
+import os
 
-def fetch_requirements(path):
+
+_build_mode = os.getenv("OPENLLAMA2_BUILD_MODE", "")
+
+
+def _is_nightly():
+    return _build_mode.lower() == "nightly"
+
+
+def _fetch_requirements(path):
     with open(path, "r") as fd:
         return [r.strip() for r in fd.readlines()]
 
 
-def fetch_readme():
+def _fetch_readme():
     with open("README.md", encoding="utf-8") as f:
         return f.read()
 
 
-def fetch_version():
+def _fetch_version():
     with open("version.txt", "r") as f:
-        return f.read().strip()
+        raw_version_number = f.read().strip()
+        return (
+            f'{raw_version_number}{datetime.today().strftime("b%Y%m%d.dev0")}' if _is_nightly() else raw_version_number
+        )
+
+
+def _fetch_package_name():
+    return "openllama2-nightly" if _is_nightly() else "openllama2"
 
 
 setup(
-    name="openllama2",
-    version=fetch_version(),
+    name=_fetch_package_name(),
+    version=_fetch_version(),
     packages=find_packages(
         exclude=(
             "data",
@@ -26,10 +43,10 @@ setup(
             "examples",
         )
     ),
-    description="A LLaMA2 implementation",
-    long_description=fetch_readme(),
+    description="A Ray-based High-performance LLaMA2 RLHF framework.",
+    long_description=_fetch_readme(),
     long_description_content_type="text/markdown",
-    install_requires=fetch_requirements("requirements.txt"),
+    install_requires=_fetch_requirements("requirements.txt"),
     python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python :: 3",
