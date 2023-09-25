cd9d30fb79f8ca5b2f781cbaa440600359bf0f39
wuxibin <wuxibin@bytedance.com>
2023-09-25 15:22:36 +0800

    support batch inference

--- examples/inference.py
+++ examples/inference.py
@@ -1,8 +1,13 @@
 import argparse
+import json
+import os
 from collections import OrderedDict
 
 import torch
+from torch import distributed as dist
+from tqdm import tqdm
 
+from openllama2.datasets import PromptDataset, SFTDataset
 from openllama2.models import Actor, RewardModel
 from openllama2.utils import blending_datasets, get_strategy, get_tokenizer
 
@@ -10,6 +15,77 @@ from openllama2.utils import blending_datasets, get_strategy, get_tokenizer
 #     replace_llama_attn_with_flash_attn,
 # )
 # replace_llama_attn_with_flash_attn()
+
+
+def batch_generate(model, tokenizer, strategy):
+    args = strategy.args
+
+    # tokenizer
+    def tokenize_fn(texts):
+        batch = tokenizer(
+            texts,
+            return_tensors="pt",
+            max_length=args.prompt_max_len,
+            padding=True,
+            truncation=True,
+        )
+        return {k: v.to(torch.cuda.current_device()) for k, v in batch.items()}
+
+    prompts_data = blending_datasets(
+        args.prompt_data,
+        "1.0",
+        strategy,
+        args.seed,
+        return_eval=False,
+    )
+    prompts_dataset = PromptDataset(prompts_data, strategy)
+    prompts_dataloader = strategy.setup_dataloader(prompts_dataset, args.batch_size, True, False)
+    pbar = tqdm(
+        prompts_dataloader,
+        disable=not strategy.is_rank_0(),
+    )
+
+    output_dataset = []
+    for prompts in pbar:
+        inputs = tokenize_fn(prompts)
+        outputs = model.generate(
+            **inputs,
+            max_length=args.max_len,
+            do_sample=True,
+            top_p=0.9,
+            early_stopping=True,
+            num_beams=1,
+            temperature=0.5,
+            repetition_penalty=1.2,
+            pad_token_id=tokenizer.pad_token_id,
+            eos_token_id=tokenizer.eos_token_id,
+        )
+        outputs = tokenizer.batch_decode(outputs[0], skip_special_tokens=True)
+        for prompt, output in zip(prompts, outputs):
+            strategy.print(output)
+            output = output[len(prompt) :].replace(r"\n", "\n")
+            output_dataset.append({"prompt": prompt, "output": output})
+
+    with open(args.output_path + str(strategy.get_rank()), "w") as f:
+        json.dump(output_dataset, f)
+
+    # wait unitl all processes generate done
+    dist.barrier()
+
+    # concate multiple output files in rank 0
+    if strategy.is_rank_0():
+        output_dataset = []
+
+        world_size = dist.get_world_size()
+        files = [args.output_path + str(rank) for rank in range(world_size)]
+        for file in files:
+            with open(file, "r") as infile:
+                data = json.load(infile)
+                output_dataset += data
+            os.remove(file)
+
+        with open(args.output_path, "w") as f:
+            json.dump(output_dataset, f)
 
 
 def generate(args):
@@ -33,6 +109,9 @@ def generate(args):
     model = strategy.prepare(model)
 
     model.eval()
+    if args.prompt_data and args.output_path:
+        return batch_generate(model, tokenizer, strategy)
+
     if args.ta_prompt:
         with open(args.ta_prompt, "r") as f:
             user_prompt = f.read()
@@ -135,12 +214,19 @@ if __name__ == "__main__":
     parser.add_argument("--eval_task", type=str, default=None)
     parser.add_argument("--pretrain", type=str, default=None)
     parser.add_argument("--load_model", type=str, default=None)
+    parser.add_argument("--prompt_max_len", type=int, default=1024)
     parser.add_argument("--max_len", type=int, default=2048)
     parser.add_argument("--zero_stage", type=int, default=0)
     parser.add_argument("--local_rank", type=int, default=-1, help="local_rank for deepspeed")
     parser.add_argument("--bf16", action="store_true", default=False)
     parser.add_argument("--inference_tp_size", type=int, default=1)
     parser.add_argument("--ta_prompt", type=str, default=None)
+
+    # batch inference
+    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--prompt_data", type=str, default=None)
+    parser.add_argument("--output_path", type=str, default=None)
+
     args = parser.parse_args()
     if args.eval_task and args.eval_task == "generate":
         generate(args)
