b9517c56e6646a3ae39aa574dccc8cc5f0fd4354
hijkzzz <janhu9527@gmail.com>
2023-12-21 00:44:30 +0000

    beta=0.1 is better than 0.5 for dpo

--- examples/scripts/train_dpo_llama.sh
+++ examples/scripts/train_dpo_llama.sh
@@ -13,7 +13,7 @@ read -r -d '' training_commands <<EOF
      --max_epochs 1 \
      --max_len 2048 \
      --zero_stage 3 \
-     --beta 0.5 \
+     --beta 0.1 \
      --learning_rate 5e-7 \
      --dataset Anthropic/hh-rlhf,tasksource/oasst1_pairwise_rlhf_reward,lmsys/chatbot_arena_conversations,openai/webgpt_comparisons \
      --dataset_probs 0.72,0.08,0.12,0.08 \
