19e4d0c618c73882de5d3f8a8a6333bfb5825368
hijkzzz <janhu9527@gmail.com>
2023-12-21 00:48:46 +0000

    upgrade container to 23.12

--- examples/scripts/docker_run.sh
+++ examples/scripts/docker_run.sh
@@ -1,7 +1,7 @@
 set -x
 
 PROJECT_PATH=$(cd $(dirname $0)/../../; pwd)
-IMAGE_NAME="nvcr.io/nvidia/pytorch:23.08-py3"
+IMAGE_NAME="nvcr.io/nvidia/pytorch:23.12-py3"
 
 docker run --runtime=nvidia -it --rm --shm-size="10g" --cap-add=SYS_ADMIN \
  	-v $PROJECT_PATH:/openrlhf -v  $HOME/.cache:/root/.cache -v  $HOME/.bash_history2:/root/.bash_history \
