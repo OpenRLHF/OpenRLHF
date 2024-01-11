c08f017504b7ed2713ec363ec2dbb23525770f38
hijkzzz <janhu9527@gmail.com>
2024-01-11 05:15:40 +0000

    fix peft zero3 save ckpt

--- openrlhf/utils/deepspeed.py
+++ openrlhf/utils/deepspeed.py
@@ -278,8 +278,11 @@ class DeepspeedStrategy(ABC):
             # only save peft weights https://github.com/microsoft/DeepSpeed/issues/4295
             if isinstance(model_to_save, PeftModel):
                 output_state_dict = get_peft_model_state_dict(model_to_save, output_state_dict)
-            # save model
-            model_to_save.save_pretrained(output_dir, state_dict=output_state_dict, **kwargs)
+                model_to_save.save_pretrained(output_dir, **kwargs)
+                torch.save(output_state_dict, os.path.join(output_dir, "adapter_model.bin"))
+            else:
+                # save model
+                model_to_save.save_pretrained(output_dir, state_dict=output_state_dict, **kwargs)
 
             # save config
             output_config_file = os.path.join(output_dir, "config.json")
