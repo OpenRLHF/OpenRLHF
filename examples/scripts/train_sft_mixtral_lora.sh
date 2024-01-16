f4913e60f761d333ca129bc2cad4f6f4ad36532c
hijkzzz <janhu9527@gmail.com>
2024-01-16 13:08:48 +0000

    fix mixtral script

--- examples/scripts/train_sft_mixtral_lora.sh
+++ examples/scripts/train_sft_mixtral_lora.sh
@@ -20,7 +20,7 @@ read -r -d '' training_commands <<EOF
     --learning_rate 5e-6 \
     --gradient_checkpointing \
     --lora_rank 64 \
-    --lora_alpha 64 \
+    --lora_alpha 16 \
     --disable_trace_cache \
     --aux_loss_coef 0.001
 EOF
