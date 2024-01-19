aaadeaa3897f9437dbb6a276c00b1d4e985876d9
hijkzzz <janhu9527@gmail.com>
2024-01-19 08:02:06 +0000

    increase batch size for mixtral

--- examples/scripts/train_sft_mixtral_lora.sh
+++ examples/scripts/train_sft_mixtral_lora.sh
@@ -9,7 +9,7 @@ read -r -d '' training_commands <<EOF
     --dataset Open-Orca/OpenOrca \
     --dataset_probs 1.0 \
     --train_batch_size 128 \
-    --micro_train_batch_size 1 \
+    --micro_train_batch_size 4 \
     --max_samples 500000 \
     --pretrain mistralai/Mixtral-8x7B-v0.1 \
     --save_path ./ckpt/mixtral_sft\
@@ -19,7 +19,7 @@ read -r -d '' training_commands <<EOF
     --zero_stage 3 \
     --max_epochs 1 \
     --bf16 \
-    --flash_attn \
+    --gradient_checkpointing \
     --learning_rate 5e-6 \
     --lora_rank 64 \
     --lora_alpha 32 \
