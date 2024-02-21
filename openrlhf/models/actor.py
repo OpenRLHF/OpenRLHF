11e8b215b63779fa717eed24f8b27e47cbc164e6
hijkzzz <janhu9527@gmail.com>
2024-02-21 12:13:48 +0000

    #217 fix position_ids

--- openrlhf/models/actor.py
+++ openrlhf/models/actor.py
@@ -175,7 +175,10 @@ class Actor(nn.Module):
         return_output=False,
     ) -> torch.Tensor:
         """Returns action log probs"""
-        output = self.model(sequences, attention_mask=attention_mask)
+        # https://github.com/OpenLLMAI/OpenRLHF/issues/217
+        position_ids = attention_mask.long().cumsum(-1) - 1
+        position_ids.masked_fill_(attention_mask == 0, 1)
+        output = self.model(sequences, attention_mask=attention_mask, position_ids=position_ids)
         log_probs = log_probs_from_logits(output["logits"][:, :-1, :], sequences[:, 1:])
 
         if return_output:
--- openrlhf/models/model.py
+++ openrlhf/models/model.py
@@ -198,9 +198,11 @@ def _get_reward_model(base_pretrained_model, base_llm_model):
             attention_mask: Optional[torch.Tensor] = None,
             return_output=False,
         ) -> torch.Tensor:
+            # https://github.com/OpenLLMAI/OpenRLHF/issues/217
+            position_ids = attention_mask.long().cumsum(-1) - 1
+            position_ids.masked_fill_(attention_mask == 0, 1)
             outputs = getattr(self, self.base_model_prefix)(
-                input_ids,
-                attention_mask=attention_mask,
+                input_ids, attention_mask=attention_mask, position_ids=position_ids
             )
             last_hidden_states = outputs["last_hidden_state"]
             values = self.value_head(last_hidden_states).squeeze(-1)
@@ -257,9 +259,13 @@ def _get_critic_model(base_pretrained_model, base_llm_model):
             attention_mask: Optional[torch.Tensor] = None,
             return_output=False,
         ) -> torch.Tensor:
+            # https://github.com/OpenLLMAI/OpenRLHF/issues/217
+            position_ids = attention_mask.long().cumsum(-1) - 1
+            position_ids.masked_fill_(attention_mask == 0, 1)
             outputs = getattr(self, self.base_model_prefix)(
                 input_ids,
                 attention_mask=attention_mask,
+                position_ids=position_ids,
             )
             last_hidden_states = outputs["last_hidden_state"]
             values = self.value_head(last_hidden_states).squeeze(-1)[:, :-1]
