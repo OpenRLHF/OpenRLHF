1fc0ff253c0620cdd628717d77b90d889ec991e7
root <dongsjtu@163.com>
2024-04-09 14:51:29 +0800

    fix kd loss

--- openrlhf/trainer/kd_trainer.py
+++ openrlhf/trainer/kd_trainer.py
@@ -129,7 +129,7 @@ class KDTrainer(ABC):
 
                 with torch.no_grad():
                     teacher_logits = self.teacher_model(inputs, attention_mask=attention_mask, return_output=True)['logits']
-                    distil_loss = self.kd_loss(output.logits, teacher_logits, labels)
+                distil_loss = self.kd_loss(output.logits, teacher_logits, labels)
 
                 loss = gpt_loss * (1 - self.args.kd_coef) + distil_loss * self.args.kd_coef
                 self.strategy.backward(loss, self.model, self.optimizer)
