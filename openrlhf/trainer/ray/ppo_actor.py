5bf1a7b6f0f951252ef99dd1e1fc97948a32ca10
hijkzzz <janhu9527@gmail.com>
2024-05-06 07:30:24 +0000

    fix lora_dropout

--- openrlhf/trainer/ray/ppo_actor.py
+++ openrlhf/trainer/ray/ppo_actor.py
@@ -157,7 +157,7 @@ class ActorModelRayActor(BasePPORole):
             lora_rank=strategy.args.lora_rank,
             lora_alpha=strategy.args.lora_alpha,
             target_modules=strategy.args.target_modules,
-            lora_alpha=strategy.args.lora_alpha,
+            lora_dropout=strategy.args.lora_dropout,
             ds_config=strategy.get_ds_train_config(is_actor=True),
         )
 
