021f634d330a02cdbeed56bd24ed96ab6ba13558
Xianyu2 <xianyuai@openllmai.top>
2024-06-08 12:58:07 +0000

    update dockerfile

--- examples/scripts/train_ppo_llama_ray_slurm.sh
+++ examples/scripts/train_ppo_llama_ray_slurm.sh
@@ -15,7 +15,7 @@
 
 # project settings
 PROJECT_PATH=$(cd ../../; realpath .)
-IMAGE_NAME="nvcr.io/nvidia/pytorch:23.12-py3"
+IMAGE_NAME="nvcr.io/nvidia/pytorch:24.2-py3"
 MOUNT="$PROJECT_PATH:/openrlhf,$HOME/.cache:/root/.cache,/dev/null:/root/.bashrc"
 
 JOBLOG="$(realpath .)/logs/train_ppo_llama_ray-$SLURM_JOB_ID.log"
@@ -35,7 +35,7 @@ echo "IP Head: $ip_head"  &>> ${JOBLOG}
 
 echo "STARTING HEAD at $node_1"  &>> ${JOBLOG}
 srun --nodes=1 --ntasks=1 -w "$node_1" --container-image="$IMAGE_NAME" --container-mounts="$MOUNT" bash -c \
-  && pip install ray[default] \
+  && pip install ray[default]==2.23.0 \
   && /root/.local/bin/ray start --head --node-ip-address=$ip --port=$port --block" &>> ${JOBLOG} &
 sleep 10s
 
@@ -44,7 +44,7 @@ for ((i = 1; i < worker_num; i++)); do
   node_i=${nodes_array[$i]}
   echo "STARTING WORKER $i at $node_i"  &>> ${JOBLOG}
   srun --nodes=1 --ntasks=1 -w "$node_i" --container-image="$IMAGE_NAME" --container-mounts="$MOUNT" bash -c \
-    && pip install ray[default] \
+    && pip install ray[default]==2.23.0 \
     && /root/.local/bin/ray start --address "$ip_head" --block" &>> ${JOBLOG} &
   sleep 1s;
 done
