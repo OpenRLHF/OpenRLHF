7adaedc2206fa1bcb8eda1addfa7729c06a21c12
Xianyu2 <xianyuai@openllmai.top>
2024-06-16 03:53:34 +0000

    Fix https://github.com/OpenLLMAI/OpenRLHF/issues/325

--- openrlhf/models/actor.py
+++ openrlhf/models/actor.py
@@ -152,7 +152,7 @@ class Actor(nn.Module):
         first_token_indices = attention_mask.long().argmax(dim=1, keepdim=True)
 
         # For Llama3 and Qwen2 models, there are some eos_tokens in the middle of the prompt.
-        mask = torch.arange(seq_length).unsqueeze(0).expand(sequences.size(0), -1)
+        mask = torch.arange(seq_length).unsqueeze(0).expand(sequences.size(0), -1).to(device=sequences.device)
         mask = (mask <= eos_indices) & (mask >= first_token_indices)
 
         attention_mask.masked_fill_(mask, 1)
