31510f2404932b7349ba0bae3abfec7fc7194859
Xianyu2 <xianyuai@openllmai.top>
2024-06-17 14:37:51 +0000

    update

--- openrlhf/models/actor.py
+++ openrlhf/models/actor.py
@@ -149,15 +149,19 @@ class Actor(nn.Module):
         #             break
         #
         eos_indices = seq_length - attention_mask.long().fliplr().argmax(dim=1, keepdim=True).clamp(min=1)
-        first_token_indices = attention_mask.long().argmax(dim=1, keepdim=True)
-
-        # For Llama3 and Qwen2 models, there are some eos_tokens in the middle of the prompt.
-        mask = torch.arange(seq_length).unsqueeze(0).expand(sequences.size(0), -1).to(device=sequences.device)
-        attention_mask = (mask <= eos_indices) & (mask >= first_token_indices).to(dtype=torch.long)
+        attention_mask.scatter_(dim=1, index=eos_indices, value=1)
         sequences.scatter_(dim=1, index=eos_indices, value=eos_token_id)
 
+        # For Llama3 and Qwen2 models, there are some eos_tokens in the middle of the prompt.
+        first_token_indices = attention_mask.long().argmax(dim=1, keepdim=True)
+        mask = torch.arange(seq_length).unsqueeze(0).expand(sequences.size(0), -1).to(device=sequences.device)
+        mask = (mask <= eos_indices) & (mask >= first_token_indices)
+        attention_mask.masked_fill_(mask, 1)
+
         # in RL, state_i (current token) + action_i (next token) -> state_i+1 (next token)
-        action_mask = attention_mask[:, input_len - 1 : -1]
+        state_seq = sequences[:, input_len - 1 : -1]
+        # we only calculate the loss of state_i != eos | pad
+        action_mask = state_seq.ne(eos_token_id) & state_seq.ne(pad_token_id)
         return sequences, attention_mask, action_mask
 
     def forward(
