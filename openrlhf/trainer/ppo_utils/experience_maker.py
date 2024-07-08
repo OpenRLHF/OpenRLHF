10e78591d5e0e5646766a17e5071d94b077a22b4
Xianyu2 <xianyuai@openllmai.top>
2024-07-08 00:04:48 +0000

    fix self.strategy.args.perf:

--- openrlhf/trainer/ppo_utils/experience_maker.py
+++ openrlhf/trainer/ppo_utils/experience_maker.py
@@ -321,9 +321,9 @@ class RemoteExperienceMaker(NaiveExperienceMaker):
             }
 
             if self.strategy.args.perf:
-                info["generate_time"] = torch.full((micro_rollout_batch_size,), generate_time, device=device)
-                info["actor_time"] = torch.full((micro_rollout_batch_size,), actor_time, device=device)
-                info["wait_time"] = torch.full((micro_rollout_batch_size,), wait_time, device=device)
+                info["generate_time"] = torch.full((action_mask_cpu.size(0),), generate_time, device=device)
+                info["actor_time"] = torch.full((action_mask_cpu.size(0),), actor_time, device=device)
+                info["wait_time"] = torch.full((action_mask_cpu.size(0),), wait_time, device=device)
 
             experience = Experience(
                 sequences_cpu,
