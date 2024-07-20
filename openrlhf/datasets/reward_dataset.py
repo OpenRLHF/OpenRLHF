a9030f6a217b4fce930237c99269cfaedf6d5d16
xiaoxigua999 <xiaoxigua999@icloud.com>
2024-07-20 03:22:03 +0000

    fix

--- openrlhf/datasets/reward_dataset.py
+++ openrlhf/datasets/reward_dataset.py
@@ -192,8 +192,8 @@ class RewardDataset(Dataset):
         chosen_ids = []
         chosen_att_masks = []
         chosen_seq_lens = []
-        rejected_id = []
-        rejected_att_mask = []
+        rejected_ids = []
+        rejected_att_masks = []
         rejected_seq_lens = []
         index = 1
         for chosen_id, chosen_mask, reject_id, rejects_mask, extra in item_list:
@@ -201,22 +201,22 @@ class RewardDataset(Dataset):
             chosen_att_masks.append(torch.ones_like(chosen_id.flatten()) * index)
             chosen_seq_lens.append(len(chosen_id.flatten()))
 
-            rejected_id.append(reject_id.flatten())
-            rejected_att_mask.append(torch.ones_like(reject_id.flatten()) * index)
+            rejected_ids.append(reject_id.flatten())
+            rejected_att_masks.append(torch.ones_like(reject_id.flatten()) * index)
             rejected_seq_lens.append(len(reject_id.flatten()))
             extras.append(extra)
             index += 1
 
         # Concatenate all tensors into a single row
         chosen_ids = torch.cat(chosen_ids, dim=0)
-        chosen_att_mask = torch.cat(chosen_att_masks, dim=0)
-        rejected_id = torch.cat(rejected_id, dim=0)
-        rejected_att_mask = torch.cat(rejected_att_mask, dim=0)
+        chosen_att_masks = torch.cat(chosen_att_masks, dim=0)
+        rejected_ids = torch.cat(rejected_ids, dim=0)
+        rejected_att_masks = torch.cat(rejected_att_masks, dim=0)
 
         # padding
         packed_input_ids = zero_pad_sequences(
-            [chosen_ids, rejected_id], side="right", value=self.tokenizer.pad_token_id
+            [chosen_ids, rejected_ids], side="right", value=self.tokenizer.pad_token_id
         )
-        packed_attention_masks = zero_pad_sequences([chosen_att_mask, rejected_att_mask], side="right")
+        packed_attention_masks = zero_pad_sequences([chosen_att_masks, rejected_att_masks], side="right")
         packed_seq_lens = chosen_seq_lens + rejected_seq_lens
         return packed_input_ids, packed_attention_masks, packed_seq_lens, extras
