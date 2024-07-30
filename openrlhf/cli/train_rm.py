ab2d972d3de9e78e395782ab3390f7b6ca0a6962
xiaoxigua999 <xiaoxigua999@icloud.com>
2024-07-30 12:00:53 +0000

    update

--- openrlhf/cli/train_rm.py
+++ openrlhf/cli/train_rm.py
@@ -99,7 +99,7 @@ def train(args):
     consumed_samples = 0
     start_epoch = 0
     if args.load_checkpoint and os.path.exists(args.ckpt_path):
-        _, states = strategy.load_ckpt(model.model, args.ckpt_path)
+        _, states = strategy.load_ckpt(model, args.ckpt_path)
         consumed_samples = states["consumed_samples"]
         start_epoch = states["epoch"]
         strategy.print(
