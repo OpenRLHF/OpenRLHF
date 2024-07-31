3040fdc97c3e96b4d4ecd9f7857eb8507c08caaa
xiaoxigua999 <xiaoxigua999@icloud.com>
2024-07-31 01:13:18 +0000

    update

--- examples/scripts/train_ppo_llama_with_remote_rm.sh
+++ examples/scripts/train_ppo_llama_with_remote_rm.sh
@@ -1,5 +1,14 @@
 set -x 
 
+# python -m openrlhf.cli.serve_rm \
+#     --reward_pretrain OpenRLHF/Llama-3-8b-rm-700k \
+#     --port 5000 \
+#     --bf16 \
+#     --flash_attn \
+#     --normalize_reward \
+#     --max_len 8192 \
+#     --batch_size 16
+
 ray job submit --address="http://127.0.0.1:8265" \
    --runtime-env-json='{"working_dir": "/openrlhf"}' \
    -- python3 -m openrlhf.cli.train_ppo_ray \
