acd89d5bfffd57cb3a4c06a8ed7709eceaaac681
LSX-Sneakerprogrammer <929170655@qq.com>
2024-10-07 14:29:09 +0800

    add grpo trainer

---
+++ openrlhf/trainer/grpo_trainer.py
@@ -0,0 +1,337 @@
+import math
+import os.path
+from abc import ABC
+from typing import Any, Callable, Dict, List, Optional, Union
+
+import ray
+import torch
+import torch.nn as nn
+from torch import Tensor
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from openrlhf.models import Actor, GRPOPolicyLoss
+from openrlhf.models.utils import masked_mean
+from openrlhf.utils.distributed_sampler import DistributedSampler
+
+from .ppo_utils import AdaptiveKLController, FixedKLController
+
+from .grpo_utils import GRPOExperience, GRPOExperienceMaker, GRPOReplayBuffer
+
+
+class GRPOTrainer(ABC):
+    """
+        Trainer for GRPO algorithm.
+
+    Args:
+        strategy (Strategy): the strategy to use for training
+        actor (Actor): the actor model in grpo algorithm
+        reward_model (nn.Module): the reward model in rlhf algorithm to make reward of sentences
+        initial_model (Actor): the initial model in rlhf algorithm to generate reference logits to limit the update of actor
+        actor_optim (Optimizer): the optimizer to use for actor model
+        kl_coef (float, defaults to 0.1): the coefficient of kl divergence loss
+        train_batch_size (int, defaults to 8): the batch size to use for training
+        buffer_limit (int, defaults to 0): the max_size limitation of replay buffer
+        buffer_cpu_offload (bool, defaults to True): whether to offload replay buffer to cpu
+        eps_clip (float, defaults to 0.2): the clip coefficient of policy loss
+        experience_batch_size (int, defaults to 8): the batch size to use for experience generation
+        max_epochs (int, defaults to 1): the number of epochs of training process
+        tokenizer (Callable, optional): the tokenizer to use for tokenizing the input
+        sample_replay_buffer (bool, defaults to False): whether to sample from replay buffer
+        dataloader_pin_memory (bool, defaults to True): whether to pin memory for data loader
+        callbacks (List[Callback], defaults to []): the callbacks to call during training process
+        generate_kwargs (dict, optional): the kwargs to use while model generating
+        remote_rm_url (str, optional): function for reward model api
+    """
+
+    def __init__(
+        self,
+        strategy,
+        actor: Actor,
+        reward_model: nn.Module,
+        initial_model: Actor,
+        ema_model: Actor,
+        actor_optim: Optimizer,
+        actor_scheduler,
+        ema_beta: float = 0.992,
+        init_kl_coef: float = 0.001,
+        kl_target: float = None,
+        kl_horizon: int = 10000,
+        micro_train_batch_size: int = 8,
+        buffer_limit: int = 0,
+        buffer_cpu_offload: bool = True,
+        eps_clip: float = 0.2,
+        micro_rollout_batch_size: int = 8,
+        gradient_checkpointing: bool = False,
+        max_epochs: int = 1,
+        max_norm: float = 1.0,
+        tokenizer: Optional[Callable[[Any], dict]] = None,
+        prompt_max_len: int = 128,
+        n_responses: int = 8,
+        dataloader_pin_memory: bool = True,
+        remote_rm_url: str = None,
+        reward_fn: Callable[[List[torch.Tensor]], torch.Tensor] = None,
+        **generate_kwargs,
+    ) -> None:
+        assert (
+            not isinstance(reward_model, List) or len(reward_model) == 1 or reward_fn is not None
+        ), "reward_fn must be specified if using multiple reward models"
+
+        super().__init__()
+        self.strategy = strategy
+        self.args = strategy.args
+        self.micro_rollout_batch_size = micro_rollout_batch_size
+        self.max_epochs = max_epochs
+        self.tokenizer = tokenizer
+        self.generate_kwargs = generate_kwargs
+        self.dataloader_pin_memory = dataloader_pin_memory
+        self.max_norm = max_norm
+        self.micro_train_batch_size = micro_train_batch_size
+        self.kl_target = kl_target
+        self.prompt_max_len = prompt_max_len
+        self.n_responses = n_responses
+        self.ema_beta = ema_beta
+        self.gradient_checkpointing = gradient_checkpointing
+        self.reward_fn = reward_fn
+
+        self.actor = actor
+        self.reward_model = reward_model
+        self.remote_rm_url = remote_rm_url
+        self.initial_model = initial_model
+        self.ema_model = ema_model
+        self.actor_optim = actor_optim
+        self.actor_scheduler = actor_scheduler
+
+        self.actor_loss_fn = GRPOPolicyLoss(eps_clip)
+
+        # Mixtral 8x7b
+        self.aux_loss = self.args.aux_loss_coef > 1e-8
+
+        if self.kl_target:
+            self.kl_ctl = AdaptiveKLController(init_kl_coef, kl_target, kl_horizon)
+        else:
+            self.kl_ctl = FixedKLController(init_kl_coef)
+        self.experience_maker = GRPOExperienceMaker(
+            actor,
+            reward_model,
+            initial_model,
+            tokenizer,
+            prompt_max_len,
+            n_responses,
+            strategy,
+            remote_rm_url,
+            reward_fn,
+        )
+        self.replay_buffer = GRPOReplayBuffer(micro_train_batch_size, n_responses, buffer_limit, buffer_cpu_offload)
+
+        self._wandb = None
+        if self.strategy.args.use_wandb and self.strategy.is_rank_0():
+            import wandb
+
+            self._wandb = wandb
+            if not wandb.api.api_key:
+                wandb.login(key=strategy.args.use_wandb)
+            wandb.init(
+                entity=strategy.args.wandb_org,
+                project=strategy.args.wandb_project,
+                group=strategy.args.wandb_group,
+                name=strategy.args.wandb_run_name,
+                config=strategy.args.__dict__,
+                reinit=True,
+            )
+
+            wandb.define_metric("train/global_step")
+            wandb.define_metric("train/*", step_metric="train/global_step", step_sync=True)
+            wandb.define_metric("eval/epoch")
+            wandb.define_metric("eval/*", step_metric="eval/epoch", step_sync=True)
+
+    def fit(
+        self,
+        args,
+        prompts_dataloader,
+        consumed_samples=0,
+        num_update_steps_per_episodes=1,
+    ) -> None:
+        num_rollouts_per_episodes = (
+            num_update_steps_per_episodes * args.train_batch_size // args.max_epochs // args.rollout_batch_size
+        )
+        update_timesteps = args.rollout_batch_size // (self.strategy.world_size * self.micro_rollout_batch_size)
+
+        # get eval and save steps
+        if args.eval_steps == -1:
+            args.eval_steps = num_rollouts_per_episodes  # Evaluate once per epoch
+        if args.save_steps == -1:
+            args.save_steps = float("inf")  # do not save ckpt
+
+        self.prompts_dataloader = prompts_dataloader
+
+        # Restore step and start_epoch
+        steps = consumed_samples // args.rollout_batch_size * update_timesteps + 1
+        start_episode = consumed_samples // args.rollout_batch_size // num_rollouts_per_episodes
+        consumed_samples = consumed_samples % (num_rollouts_per_episodes * args.rollout_batch_size)
+
+        for episode in range(start_episode, args.num_episodes):
+            if isinstance(self.prompts_dataloader.sampler, DistributedSampler):
+                self.prompts_dataloader.sampler.set_epoch(
+                    episode, consumed_samples=0 if episode > start_episode else consumed_samples
+                )
+            pbar = tqdm(
+                range(self.prompts_dataloader.__len__()),
+                desc=f"Episode [{episode + 1}/{args.num_episodes}]",
+                disable=not self.strategy.is_rank_0(),
+            )
+
+            for rand_prompts in self.prompts_dataloader:
+                experiences = self.experience_maker.make_experience(rand_prompts, **self.generate_kwargs)
+                # print prompt/answer in each update step
+                if steps % update_timesteps == 0:
+                    output = self.tokenizer.batch_decode(experiences.sequences, skip_special_tokens=True)
+                    self.strategy.print(output[0])
+                self.replay_buffer.append(experiences)
+
+                if steps % update_timesteps == 0:
+                    global_steps = steps // update_timesteps
+
+                    torch.cuda.empty_cache()
+                    status = self.grpo_train()
+                    self.replay_buffer.clear()
+                    torch.cuda.empty_cache()
+
+                    if "kl" in status:
+                        self.kl_ctl.update(status["kl"], args.rollout_batch_size)
+                    pbar.set_postfix(status)
+
+                    # logs/checkpoints
+                    client_states = {"consumed_samples": global_steps * args.rollout_batch_size}
+                    self.save_logs_and_checkpoints(args, global_steps, pbar, status, client_states)
+
+                pbar.update()
+                steps = steps + 1
+
+    def grpo_train(self):
+        # replay buffer may be empty at first, we should rebuild at each training
+        dataloader = DataLoader(
+            self.replay_buffer,
+            batch_size=self.replay_buffer.sample_batch_size,
+            shuffle=True,
+            drop_last=True,
+            pin_memory=self.dataloader_pin_memory,
+            collate_fn=self.replay_buffer.collate_fn,
+        )
+        device = torch.cuda.current_device()
+
+        status_list = []
+        status_mean = {}
+        for epoch in range(self.max_epochs):
+            pbar = tqdm(
+                dataloader,
+                desc=f"Train epoch [{epoch + 1}/{self.max_epochs}]",
+                disable=not self.strategy.is_rank_0(),
+            )
+            for experience in pbar:
+                experience.to_device(device)
+                status = self.training_step(experience)
+
+                # for DP
+                # weighted mean for kl
+                if "kl" in status:
+                    status["kl"] *= status["response_length"]
+                    status = self.strategy.all_reduce(status)
+                    status["kl"] /= status["response_length"]
+
+                short_status = {}
+
+                if "policy_loss" in status:
+                    short_status = {
+                        "pg": status["policy_loss"],
+                        "rm": status["reward"],
+                        "glen": status["response_length"],
+                        "tlen": status["total_length"],
+                        "kl": status["kl"],
+                        "act_lr": status["actor_lr"],
+                    }
+
+                status_list.append(status)
+                pbar.set_postfix(short_status)
+
+        if status_list:
+            status_mean = status_list[0]
+            for m in status_list[1:]:
+                for k, v in m.items():
+                    status_mean[k] += v
+            for k in status_mean.keys():
+                status_mean[k] /= len(status_list)
+        return status_mean
+
+    def training_step(self, experience: GRPOExperience) -> Dict[str, float]:
+        self.actor.train()
+
+        num_actions = experience.action_mask.size(1)
+        # actor loss
+        action_log_probs, output = self.actor(
+            experience.sequences, num_actions, attention_mask=experience.attention_mask, return_output=True
+        )
+        # loss function
+        actor_loss, kl = self.actor_loss_fn(
+            action_log_probs,
+            experience.action_log_probs,
+            experience.base_action_log_probs,
+            experience.advantages,
+            self.kl_ctl.value,
+            action_mask=experience.action_mask,
+        )
+        kl = masked_mean(kl, experience.action_mask, dim=-1)
+        # mixtral
+        if self.aux_loss:
+            aux_loss = output.aux_loss
+        else:
+            aux_loss = 0
+        loss = actor_loss + aux_loss * self.args.aux_loss_coef
+        self.strategy.backward(loss, self.actor, self.actor_optim)
+
+        self.strategy.optimizer_step(self.actor_optim, self.actor, self.actor_scheduler, name="actor")
+        if self.ema_model:
+            self.strategy.moving_average(self.actor, self.ema_model, self.ema_beta, "cpu")
+
+        # status
+        status = {"policy_loss": actor_loss.item(), "actor_lr": self.actor_scheduler.get_last_lr()[0]}
+        status["kl"] = (
+                (kl * experience.info["response_length"]).sum() / experience.info["response_length"].sum()
+        ).item()
+        for k, v in experience.info.items():
+            status[k] = v.mean().item()
+        return status
+
+    def save_logs_and_checkpoints(self, args, global_step, step_bar, logs_dict={}, client_states={}):
+        if global_step % args.logging_steps == 0:
+            # wandb
+            if self._wandb is not None and self.strategy.is_rank_0():
+                logs = {
+                    "train/%s" % k: v
+                    for k, v in {
+                        **logs_dict,
+                        "global_step": global_step,
+                    }.items()
+                }
+                self._wandb.log(logs)
+
+        # TODO: Add evaluation mechanism for GRPO
+        if global_step % args.eval_steps == 0:
+            # self.evaluate(self.eval_dataloader, global_step)
+            pass
+        # save ckpt
+        # TODO: save best model on dev, use loss/perplexity/others on whole dev dataset as metric
+        if global_step % args.save_steps == 0:
+            tag = f"global_step{global_step}"
+            self._save_checkpoint(args, tag, client_states)
+
+    def _save_checkpoint(self, args, tag, client_states):
+        self.strategy.save_ckpt(
+            self.actor.model,
+            os.path.join(args.ckpt_path, "_actor"),
+            tag,
+            args.max_ckpt_num,
+            args.max_ckpt_mem,
+            client_states,
+        )
