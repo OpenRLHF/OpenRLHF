3f99e5b6ba41f3ac91297b00f3205dad1fbbccff
ℍ𝕠𝕝𝕝𝕠𝕨 𝕄𝕒𝕟 <hollowman@opensuse.org>
2024-10-16 01:45:02 +0300

    Change pg_options param into backend_options in _new_process_group_helper (#462)
    for PyTorch version greater than 2.6
    
    As per changes at PyTorch side:
    https://github.com/pytorch/pytorch/commit/a0c7029a75628cd5fa8df83c0de0ea98ee7fd844
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- openrlhf/utils/distributed_util.py
+++ openrlhf/utils/distributed_util.py
@@ -53,6 +53,10 @@ def init_process_group(
         # different systems (e.g. RPC) in case the store is multi-tenant.
         store = PrefixStore(group_name, store)
 
+    # NOTE: The pg_options parameter was renamed into backend_options in PyTorch 2.6.0
+    # https://github.com/pytorch/pytorch/commit/a0c7029a75628cd5fa8df83c0de0ea98ee7fd844
+    # We need to determine the appropriate parameter name based on PyTorch version
+    pg_options_param_name = "backend_options" if str(torch.__version__) >= "2.6" else "pg_options"
     pg, _ = _new_process_group_helper(
         world_size,
         rank,
@@ -60,7 +64,7 @@ def init_process_group(
         backend,
         store,
         group_name=group_name,
-        pg_options=pg_options,
+        **{pg_options_param_name: pg_options},
         timeout=timeout,
     )
 
