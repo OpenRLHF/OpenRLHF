913ee6d86d203cd070ac9867f2d668ae15508107
Hollow Man <hollowman@opensuse.org>
2024-10-30 09:50:51 +0200

    Replace deprecated/removed transformers.deepspeed module
    In latest transformers version 4.46.1, transformers.deepspeed has
    officially been removed, while for the current version 4.44.2
    OpenRLHF is using, it generates the warning. So it's better to
    update the code immediately.
    
    venv/lib/python3.10/site-packages/transformers/deepspeed.py:23:
    FutureWarning: transformers.deepspeed module is deprecated and
    will be removed in a future version. Please import deepspeed
    modules directly from transformers.integrations
      warnings.warn(
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- openrlhf/models/model.py
+++ openrlhf/models/model.py
@@ -8,7 +8,7 @@ from flash_attn.utils.distributed import all_gather
 from peft import LoraConfig, get_peft_model
 from peft.tuners.lora import LoraLayer
 from transformers import AutoConfig, AutoModel, BitsAndBytesConfig
-from transformers.deepspeed import HfDeepSpeedConfig
+from transformers.integrations.deepspeed import HfDeepSpeedConfig
 from transformers.dynamic_module_utils import get_class_from_dynamic_module
 
 from openrlhf.utils.logging_utils import init_logger
