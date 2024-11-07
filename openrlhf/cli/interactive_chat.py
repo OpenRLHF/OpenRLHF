b6f1cccdb468ccb27438e6f991952cebfbb7bd76
LYMDLUT <70597027+LYMDLUT@users.noreply.github.com>
2024-11-07 17:02:08 +0800

    Update interactive_chat.py

--- openrlhf/cli/interactive_chat.py
+++ openrlhf/cli/interactive_chat.py
@@ -72,11 +72,12 @@ def generate(args):
             pad_token_id=tokenizer.pad_token_id,
             eos_token_id=tokenizer.eos_token_id,
         )
-        generated_ids = outputs[0][:, input_ids.shape[1]:]
-        response = tokenizer.batch_decode(
-        generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
-        )[0]
+
         if args.apply_chat_template:
+            generated_ids = outputs[0][:, input_ids.shape[1]:]
+            response = tokenizer.batch_decode(
+            generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )[0]
             conversations.append({"role": "assistant", "content": response})
         else:
             user_prompt = tokenizer.batch_decode(outputs[0], skip_special_tokens=True)[0]
