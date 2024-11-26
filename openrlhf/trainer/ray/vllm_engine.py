01c5feb2ebc52efabde2664fa8b9024adcc94ec5
Hollow Man <hollowman@opensuse.org>
2024-11-26 21:54:51 +0200

    Use worker_cls when vLLM version > 0.6.4.post1
    `worker_module_name` and `worker_class_name` is no longer supported.
    
    Refer to https://github.com/vllm-project/vllm/pull/10555
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- openrlhf/trainer/ray/vllm_engine.py
+++ openrlhf/trainer/ray/vllm_engine.py
@@ -30,18 +30,22 @@ class LLMRayActor:
             # See the patch https://github.com/vllm-project/vllm/commit/479d69fad0538f04cb22bf13e76ff91cfeb8a4e5
             kwargs["worker_use_ray"] = True
 
-            if vllm.__version__ > "0.4.1":
-                RayWorkerWrapperPath = vllm.executor.ray_utils
+            if vllm.__version__ > "0.6.4.post1":
+                # https://github.com/vllm-project/vllm/pull/10555
+                kwargs["worker_cls"] = "openrlhf.trainer.ray.vllm_worker_wrap.WorkerWrap"
             else:
-                RayWorkerWrapperPath = vllm.engine.ray_utils
+                if vllm.__version__ > "0.4.1":
+                    RayWorkerWrapperPath = vllm.executor.ray_utils
+                else:
+                    RayWorkerWrapperPath = vllm.engine.ray_utils
 
-            class RayWorkerWrapper(RayWorkerWrapperPath.RayWorkerWrapper):
-                def __init__(self, *args, **kwargs) -> None:
-                    kwargs["worker_module_name"] = "openrlhf.trainer.ray.vllm_worker_wrap"
-                    kwargs["worker_class_name"] = "WorkerWrap"
-                    super().__init__(*args, **kwargs)
+                class RayWorkerWrapper(RayWorkerWrapperPath.RayWorkerWrapper):
+                    def __init__(self, *args, **kwargs) -> None:
+                        kwargs["worker_module_name"] = "openrlhf.trainer.ray.vllm_worker_wrap"
+                        kwargs["worker_class_name"] = "WorkerWrap"
+                        super().__init__(*args, **kwargs)
 
-            RayWorkerWrapperPath.RayWorkerWrapper = RayWorkerWrapper
+                RayWorkerWrapperPath.RayWorkerWrapper = RayWorkerWrapper
 
         self.llm = vllm.LLM(*args, **kwargs)
 
