86aed269e9daf13c5ddb8cd781fe82cbfdeade0d
xiaoxigua999 <xiaoxigua999@icloud.com>
2025-01-06 05:40:40 +0000

    Fix reinforce++ script

--- examples/scripts/train_reinforce_llama_ray.sh
+++ examples/scripts/train_reinforce_llama_ray.sh
@@ -16,7 +16,7 @@ ray job submit --address="http://127.0.0.1:8265" \
    --pretrain OpenRLHF/Llama-3-8b-sft-mixture \
    --reward_pretrain OpenRLHF/Llama-3-8b-rm-700k \
    --save_path /openrlhf/examples/test_scripts/checkpoint/llama3-8b-rlhf \
-   --micro_train_batch_size 8 \
+   --micro_train_batch_size 16 \
    --train_batch_size 128 \
    --micro_rollout_batch_size 32 \
    --rollout_batch_size 1024 \
@@ -29,7 +29,6 @@ ray job submit --address="http://127.0.0.1:8265" \
    --zero_stage 3 \
    --bf16 \
    --actor_learning_rate 5e-7 \
-   --critic_learning_rate 9e-6 \
    --init_kl_coef 0.01 \
    --prompt_data OpenRLHF/prompt-collection-v0.1 \
    --input_key context_messages \
