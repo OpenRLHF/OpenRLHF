5a06164334f23c467cbe3dcfa7ef918c9931c567
xiaoxigua999 <xiaoxigua999@icloud.com>
2025-01-06 02:43:09 +0000

    Fix adapter_model.safetensors for LoRA

--- openrlhf/utils/deepspeed/deepspeed.py
+++ openrlhf/utils/deepspeed/deepspeed.py
@@ -342,6 +342,7 @@ class DeepspeedStrategy(ABC):
                         get_peft_model_state_dict(model_to_save, output_state_dict),
                         os.path.join(output_dir, "adapter_model.bin"),
                     )
+                    os.remove(os.path.join(output_dir, "adapter_model.safetensors"))
             else:
                 # save model
                 model_to_save.save_pretrained(output_dir, state_dict=output_state_dict, **kwargs)
