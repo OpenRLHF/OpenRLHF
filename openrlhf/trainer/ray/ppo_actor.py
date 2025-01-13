37fe95a74bb3aa8ef42acd4481c46d75ff8a6903
xiaoxigua999 <xiaoxigua999@icloud.com>
2025-01-13 00:57:17 +0000

    fix

--- openrlhf/trainer/ray/ppo_actor.py
+++ openrlhf/trainer/ray/ppo_actor.py
@@ -34,8 +34,6 @@ class ActorPPOTrainer(PPOTrainer):
         Args:
             vllm_engines (List, optional): vllm engines for text generation, if not specified, generate text by actor model directly. Defaults to None.
             critic_train_remote (bool, optional): whether this actor should triger corresponding critic model training. Defaults to False.
-            save_hf_ckpt (bool): Whether to save huggingface-format model weight.
-            disable_ds_ckpt (bool): Whether not to save deepspeed-format model weight. (Deepspeed model weight is used for training recovery)
         """
         super().__init__(*args, **kwargs)
         self.remote_rm_url = remote_rm_url
