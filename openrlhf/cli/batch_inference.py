aac55d73a73d2766e1374ff49e462c13783959d3
zhaochenyang20 <zhaochen20@outlook.com>
2025-01-15 01:40:41 +0000

    Add batch inference sglang for iterative DPO

--- openrlhf/cli/batch_inference.py
+++ openrlhf/cli/batch_inference.py
@@ -47,8 +47,8 @@ def batch_generate_sglang(args):
     # configure model
 
     # Note that SGLang does not support max_num_seqs
-    # and the grammar backend of SGLang and vllm has
-    # unsolved version conflicts.
+    # and the default grammar backend outlines of SGLang
+    # and vllm has unsolved version conflicts.
 
     llm = Engine(
         model_path=args.pretrain,
@@ -60,11 +60,12 @@ def batch_generate_sglang(args):
 
     # Create a sampling params object.
 
-    # TODO: (Chenyang) Check include_stop_str_in_output
-
     # SGLang strictly follows the sampling parameters of OpenAI.
     # Therefore, there is no prompt_max_len and
     # include_stop_str_in_output in sampling params.
+
+    # `include_stop_str_in_output = True` in vllm means to
+    # include <eos> in the output, which is supported by SGLang.
 
     # prompt_max_len is similar to context_length parameter in
     # SGLang engine args. But SGLang engine raises an error when
@@ -104,6 +105,11 @@ def batch_generate_sglang(args):
     for prompt, output in zip(prompts * N, outputs):
         output = output["text"]
         output_dataset.append({"input": prompt, "output": output})
+
+    # TODO: why <eos> is not included in the output?
+    eos_token = tokenizer.eos_token
+    for each in output_dataset:
+        each["output"] = each["output"] + eos_token
 
     with jsonlines.open(args.output_path, mode="w") as writer:
         writer.write_all(output_dataset)
@@ -163,127 +169,6 @@ def batch_generate_vllm(args):
 
     with jsonlines.open(args.output_path, mode="w") as writer:
         writer.write_all(output_dataset)
-
-# This function is not being used, so comment it out for now
-
-# def batch_generate(args, engine_type="sglang"):
-#     if engine_type == "sglang":
-#         from sglang import Engine
-#     elif engine_type == "vllm":
-#         from vllm import LLM, SamplingParams
-#     else:
-#         raise ValueError("Invalid engine type. Choose 'sglang' or 'vllm'.")
-
-#     # configure strategy
-#     class Empty:
-#         pass
-
-#     dummy_strategy = Empty()
-#     dummy_strategy.print = print
-#     dummy_strategy.is_rank_0 = lambda: True
-#     dummy_strategy.args = args
-
-#     # configure tokenizer
-#     tokenizer = AutoTokenizer.from_pretrained(args.pretrain, trust_remote_code=True)
-
-#     # configure model
-#     if engine_type == "sglang":
-
-#         # Note that SGLang does not support max_num_seqs
-#         # and the grammar backend of SGLang and vllm has
-#         # unsolved version conflicts.
-
-#         llm = Engine(
-#             model_path=args.pretrain,
-#             tp_size=args.tp_size,
-#             trust_remote_code=True,
-#             random_seed=args.seed,
-#             disable_radix_cache=not args.enable_prefix_caching,
-#         )
-#     else:  # vllm
-#         llm = LLM(
-#             model=args.pretrain,
-#             tensor_parallel_size=args.tp_size,
-#             trust_remote_code=True,
-#             seed=args.seed,
-#             max_num_seqs=args.max_num_seqs,
-#             enable_prefix_caching=args.enable_prefix_caching,
-#         )
-
-#     # Create a sampling params object.
-#     if engine_type == "sglang":
-
-#         # TODO: (Chenyang) Check include_stop_str_in_output
-
-#         # SGLang strictly follows the sampling parameters of OpenAI.
-#         # Therefore, there is no prompt_max_len and
-#         # include_stop_str_in_output in sampling params.
-
-#         # prompt_max_len is similar to context_length parameter in
-#         # SGLang engine args. But SGLang engine raises an error when
-#         # request length is larger than context_length, rather than
-#         # truncate the prompt and only use the last tokens in vllm.
-
-#         sampling_params = dict(
-#             max_new_tokens=args.max_new_tokens,
-#             top_p=args.top_p,
-#             temperature=args.temperature,
-#             repetition_penalty=args.repetition_penalty,
-#             skip_special_tokens=False,
-#         )
-#     else:  # vllm
-#         sampling_params = SamplingParams(
-#             max_tokens=args.max_new_tokens,
-#             top_p=args.top_p,
-#             temperature=args.temperature,
-#             repetition_penalty=args.repetition_penalty,
-#             skip_special_tokens=False,
-#             truncate_prompt_tokens=args.prompt_max_len,
-#             include_stop_str_in_output=True,
-#         )
-
-#     prompts_data = blending_datasets(
-#         args.dataset,
-#         args.dataset_probs,
-#         dummy_strategy,
-#         args.seed,
-#         return_eval=False,
-#         max_count=args.max_samples,
-#         train_split=args.dataset_split,
-#     )
-#     if args.iter is None:
-#         prompts_data = prompts_data.select(range(min(args.max_samples, len(prompts_data))))
-#     else:
-#         # for iterative generation
-#         start_idx = args.iter * args.rollout_batch_size
-#         end_idx = start_idx + args.rollout_batch_size
-#         prompts_data = prompts_data.select(range(start_idx, min(end_idx, len(prompts_data))))
-
-#     prompts_dataset = PromptDataset(prompts_data, tokenizer, dummy_strategy, input_template=args.input_template)
-#     prompts = list(prompts_dataset)
-
-#     # Conditional SFT inference
-#     if args.enable_csft:
-#         for i in range(len(prompts)):
-#             prompts[i] += args.csft_prompt.strip() + " "
-
-#     # best of n
-#     N = args.best_of_n
-#     output_dataset = []
-
-#     outputs = llm.generate(prompts * N, sampling_params)
-#     if engine_type == "sglang":
-#         for prompt, output in zip(prompts * N, outputs):
-#             output = output["text"]
-#             output_dataset.append({"input": prompt, "output": output})
-#     else:  # vllm
-#         for output in outputs:
-#             prompt = output.prompt
-#             output = output.outputs[0].text
-#             output_dataset.append({"input": prompt, "output": output})
-
-#     with jsonlines.open(args.output_path, mode="w") as writer:
-#         writer.write_all(output_dataset)
 
 
 def batch_generate_hf(args):
