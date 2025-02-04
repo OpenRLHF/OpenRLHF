03e2bf1b1f042c6f819019d52710c31f6be6b5be
ℍ𝕠𝕝𝕝𝕠𝕨 𝕄𝕒𝕟 <hollowman@opensuse.org>
2025-02-04 03:01:51 +0200

    Add support for vLLM weight sync with Ray collective communication (#704)
    Can be an alternative to / aside from #680 and fix #492, as in
    Ray collective communication lib, setting master addr/port is not needed.
    
    Current torch vLLM weight sync implementation with NCCL sometimes
    causes hang, especially on AMD GPUs, this PR tries to address this
    issue by using Ray collective communication lib, so that we can
    benefit from the blazing fast NCCL (RCCL on AMD) smoothly.
    
    https://docs.ray.io/en/latest/ray-more-libs/ray-collective.html
    
    Note that Ray CC lib would need cupy for NCCL backend.
    
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- openrlhf/trainer/ray/vllm_worker_wrap.py
+++ openrlhf/trainer/ray/vllm_worker_wrap.py
@@ -8,19 +8,30 @@ logger = init_logger(__name__)
 
 
 class WorkerWrap(Worker):
-    def init_process_group(self, master_address, master_port, rank_offset, world_size, group_name, backend="nccl"):
+    def init_process_group(self, master_address, master_port, rank_offset, world_size, group_name, backend="nccl", use_ray=False):
         """Init torch process group for model weights update"""
         assert torch.distributed.is_initialized(), f"default torch process group must be initialized"
         assert group_name != "", f"group name must not be empty"
 
         rank = torch.distributed.get_rank() + rank_offset
-        self._model_update_group = init_process_group(
-            backend=backend,
-            init_method=f"tcp://{master_address}:{master_port}",
-            world_size=world_size,
-            rank=rank,
-            group_name=group_name,
-        )
+        if use_ray:
+            import ray.util.collective as collective
+            collective.init_collective_group(
+                world_size=world_size,
+                rank=rank,
+                backend=backend,
+                group_name=group_name
+            )
+            self._model_update_group = group_name
+        else:
+            self._model_update_group = init_process_group(
+                backend=backend,
+                init_method=f"tcp://{master_address}:{master_port}",
+                world_size=world_size,
+                rank=rank,
+                group_name=group_name,
+            )
+        self._model_update_with_ray = use_ray
         print(
             f"init_process_group: master_address={master_address}, master_port={master_port}, ",
             f"rank={rank}, world_size={world_size}, group_name={group_name}",
@@ -33,7 +44,11 @@ class WorkerWrap(Worker):
 
         assert dtype == self.model_config.dtype, f"mismatch dtype: src {dtype}, dst {self.model_config.dtype}"
         weight = torch.empty(shape, dtype=dtype, device="cuda")
-        torch.distributed.broadcast(weight, 0, group=self._model_update_group)
+        if self._model_update_with_ray:
+            import ray.util.collective as collective
+            collective.broadcast(weight, 0, group_name=self._model_update_group)
+        else:
+            torch.distributed.broadcast(weight, 0, group=self._model_update_group)
 
         self.model_runner.model.load_weights(weights=[(name, weight)])
 
