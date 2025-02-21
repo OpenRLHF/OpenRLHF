fix grpo bug in kl=0 (#773)
* offload ref when kl=0

* offload ref when kl=0

* add grpo training

* add grpo scripts

* fix grpo bug in non-packing

* fix grpo bug in kl=0

openrlhf/trainer/ppo_trainer.py
@@ -346,7 +346,7 @@ class PPOTrainer(ABC):
             attention_mask = torch.cat(
                 [torch.full_like(s, i + 1) for i, s in enumerate(experience.sequences)], dim=0
             ).unsqueeze(0)
-            if self.args.use_kl_loss:
+            if self.args.use_kl_loss and experience.base_action_log_probs is not None:
                 base_action_log_probs = torch.cat(experience.base_action_log_probs, dim=0).unsqueeze(0)
         else:
             sequences = experience.sequences
@@ -355,7 +355,7 @@ class PPOTrainer(ABC):
             num_actions = experience.action_mask.size(1)
             packed_seq_lens = None
             attention_mask = experience.attention_mask
-            if self.args.use_kl_loss:
+            if self.args.use_kl_loss and experience.base_action_log_probs is not None:
                 base_action_log_probs = experience.base_action_log_probs
 
         # actor loss