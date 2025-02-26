47523a2aef55ef9f36437c1a46d1ef1816417747
LYMDLUT <70597027+LYMDLUT@users.noreply.github.com>
2025-02-26 21:12:22 +0800

    Update train_grpo_ray_hybrid_engine.sh

--- examples/scripts/train_grpo_ray_hybrid_engine.sh
+++ examples/scripts/train_grpo_ray_hybrid_engine.sh
@@ -16,7 +16,7 @@ ray job submit --address="http://127.0.0.1:8265" \
    --init_kl_coef 1e-3 \
    --gamma 1.0 \
    --use_kl_loss \
-   --use_kl_estimator_k3 \
+   --kl_estimator_type k3 \
    --advantage_estimator group_norm \
    --pretrain OpenRLHF/Llama-3-8b-sft-mixture \
    --reward_pretrain OpenRLHF/Llama-3-8b-rm-700k \
