24e01560ae50a8568e73ef25e97591f0e1c78b04
LYMDLUT <70597027+LYMDLUT@users.noreply.github.com>
2025-02-28 22:53:04 +0800

    Update train_grpo_llama_ray.sh

--- examples/scripts/train_grpo_llama_ray.sh
+++ examples/scripts/train_grpo_llama_ray.sh
@@ -28,7 +28,7 @@ ray job submit --address="http://127.0.0.1:8265" \
    --init_kl_coef 1e-3 \
    --gamma 1.0 \
    --use_kl_loss \
-   --kl_estimator_type k3_loss \
+   --kl_estimator_type k3 \
    --advantage_estimator group_norm \
    --zero_stage 3 \
    --bf16 \
