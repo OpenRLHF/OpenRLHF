010275406612ac0894625d2eef12af4268c7a473
kiaia <898134692@qq.com>
2025-03-03 21:08:27 +0000

    update predict_detail api

--- openrlhf/search_algorithm/trial_n.py
+++ openrlhf/search_algorithm/trial_n.py
@@ -8,18 +8,39 @@ from openrlhf.utils.remote_rm_utils import request_api_wrapper
 from openrlhf.search_algorithm.bestofn import get_full_trajs_vllm
 from openrlhf.search_algorithm.search_utils import DEFAULT_N, DEFAULT_TEMPERATURE, initialize_question_answer_map
 
-'''
-curl -X POST "http://localhost:1234/predict" \
--H "Content-Type: application/json" \
--d '{
-  "queries": [
-    {
-      "formal_statement": "theorem test_true : True",
-      "proof": "theorem test_true : True where\n  exact True.intro"
-    }
-  ]
-}'
-'''
+"""
+## Correct case
+curl -X POST "http://localhost:1234/predict_de
+tail" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "queries": [
+      {
+        "formal_statement": "theorem lean_workbook_plus_65302 (x y : ℝ) (h : x * y + y * x + x 
+= 1) : 2 * x * y + x = 1",
+        "proof": ":= by\n  rw [mul_comm] at h\n  linarith\n"
+      }
+    ]
+  }'
+{"rewards":[1.0],"details":[{"formal_statement":"theorem lean_workbook_plus_65302 (x y : ℝ) (h 
+: x * y + y * x + x = 1) : 2 * x * y + x = 1","verification_time":121.857168674469,"errors":[],
+"status":"unknown","complete":true,"pass":true}]}
+
+## Wrong case
+curl -X POST "http://localhost:1234/predict_detail" \ail" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "queries": [
+      {
+        "formal_statement": "theorem lean_workbook_plus_65302 (x y : ℝ) (h : x * y + y * x + x = 1) : 2 * x * y + x = 1",
+        "proof": ":= by\n  rw [mul_comm] at h\n  linarit\n"
+      }
+    ]
+  }'
+{"rewards":[-1.0],"details":[{"formal_statement":"theorem lean_workbook_plus_65302 (x y : ℝ) (h : x * y + y * x + x = 1) : 2 * x * y + x = 1","verification_time":18.54811954498291,"errors":[],"status":"unknown","complete":false,"pass":false}]}
+
+
+"""
 def verify_by_lean(api_url, statements: List[str], proofs: List[str]):
     queries = [{"formal_statement": s, "proof": p} for s, p in zip(statements, proofs)]
     scores = request_api_wrapper(api_url, {"query": queries}, score_key)
@@ -41,9 +62,9 @@ def search_vllm(queries, tokenizer, actor, critic=None, search_args=None):
         all_prompts, all_indices = [], []
         for i, traj in enumerate(trajs):
             # TODO (mukai): you can decide the format of lean_outcomes, not necessarily str
-            if lean_outcomes[i] == "success":
+            if lean_outcomes[i]['rewards'] == 1:
                 sequences[all_indices[i]] = traj
-            elif lean_outcomes[i] == "fail":
+            elif lean_outcomes[i]["rewards"] != 1:
                 # TODO: get the longest partial trajectory before the compile error
                 partial_traj = longest_partial_trajectory()
                 all_prompts.append(partial_traj)
