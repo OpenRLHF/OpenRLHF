1dfe7911182d74c400c5c8a46a90eb0d98c7a757
Fish <45708320+lxline@users.noreply.github.com>
2025-03-20 18:01:58 +0800

    Get datasets from ModelScope use `--use_ms`. (#893)
    * get dataset from modelscope
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- openrlhf/utils/utils.py
+++ openrlhf/utils/utils.py
@@ -82,6 +82,11 @@ def blending_datasets(
                 data = load_dataset(dataset, data_dir=data_dir)
                 strategy.print(f"loaded {dataset} from files")
         # remote/local folder or common file
+        elif strategy.args.use_ms:
+            from modelscope.msdatasets import MsDataset
+
+            namespace, dataset = dataset.split("/")
+            data = MsDataset.load(dataset, namespace=namespace)
         else:
             data = load_dataset(dataset, data_dir=data_dir)
             strategy.print(f"loaded {dataset} from files")
