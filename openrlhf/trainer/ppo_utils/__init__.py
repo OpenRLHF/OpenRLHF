51bcc0588663605934453ea82399fbce7e3c8c69
xiaoxigua999 <xiaoxigua999@icloud.com>
2025-03-22 06:41:18 +0000

    fix

--- openrlhf/trainer/ppo_utils/__init__.py
+++ openrlhf/trainer/ppo_utils/__init__.py
@@ -1,10 +1,9 @@
-from .experience_maker import Experience, NaiveExperienceMaker, RemoteExperienceMaker
+from .experience_maker import Experience, RemoteExperienceMaker
 from .kl_controller import AdaptiveKLController, FixedKLController
 from .replay_buffer import NaiveReplayBuffer
 
 __all__ = [
     "Experience",
-    "NaiveExperienceMaker",
     "RemoteExperienceMaker",
     "AdaptiveKLController",
     "FixedKLController",
