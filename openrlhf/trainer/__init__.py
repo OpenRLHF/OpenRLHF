1f72142ad354b90c53d1af8af816ee794b3ccf2b
xiaoxigua999 <xiaoxigua999@icloud.com>
2025-04-17 00:34:56 +0000

    fix

--- openrlhf/trainer/__init__.py
+++ openrlhf/trainer/__init__.py
@@ -1,7 +1,7 @@
 from .dpo_trainer import DPOTrainer
 from .kd_trainer import KDTrainer
 from .kto_trainer import KTOTrainer
-from .ppo_trainer import BasePPOTrainer
+from .ppo_trainer import PPOTrainer
 from .prm_trainer import ProcessRewardModelTrainer
 from .rm_trainer import RewardModelTrainer
 from .sft_trainer import SFTTrainer
@@ -10,7 +10,7 @@ __all__ = [
     "DPOTrainer",
     "KDTrainer",
     "KTOTrainer",
-    "BasePPOTrainer",
+    "PPOTrainer",
     "ProcessRewardModelTrainer",
     "RewardModelTrainer",
     "SFTTrainer",
