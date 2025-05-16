Fix attention mask

openrlhf/trainer/ppo_utils/experience_maker.py
@@ -9,7 +9,7 @@ import ray
 import torch
 
 from openrlhf.datasets.utils import zero_pad_sequences
-from openrlhf.models.utils import compute_approx_kl, compute_reward, masked_mean
+from openrlhf.models.utils import compute_approx_kl, compute_reward, masked_mean, process_sequences
 from openrlhf.trainer.ray.launcher import PPORayActorGroup
 from openrlhf.utils.logging_utils import init_logger
 from openrlhf.utils.remote_rm_utils import remote_rm_fn_ray
@@ -710,7 +710,7 @@ class RemoteExperienceMaker(ABC):
                 attention_mask.append([1] * len(input_ids) + [0] * (batch_max_input_len - len(input_ids)))
 
             sequences = torch.tensor(sequences)
-            attention_mask = torch.tensor(attention_mask)
+            attention_mask = process_sequences(sequences, eos_token_id, pad_token_id)
 
             # Create action mask based on output token positions
             action_mask = torch.zeros_like(attention_mask)