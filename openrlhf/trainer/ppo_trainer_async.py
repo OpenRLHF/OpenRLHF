bde3c535aa8855603424b618837fe8b55064f084
xiaoxigua999 <xiaoxigua999@icloud.com>
2025-05-17 08:55:59 +0000

    update

--- openrlhf/trainer/ppo_trainer_async.py
+++ openrlhf/trainer/ppo_trainer_async.py
@@ -183,7 +183,7 @@ class PPOTrainerAsync:
         from ray.util.queue import Queue
 
         # the max size is used to control the degree of off-policy
-        self.queue = Queue(maxsize=os.environ.get("OPENRLHF_ASYNC_QUEUE_SIZE", 5))
+        self.queue = Queue(maxsize=os.environ.get("OPENRLHF_ASYNC_QUEUE_SIZE", 3))
 
     def fit(
         self,
