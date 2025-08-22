7716cb9371436028e0a1e7e889b2d9819d7e0e10
Rongzhi Mai <makwin9chi@gmail.com>
2025-08-23 05:11:46 +0800

    fix remote reward model init bug in PPOTrainerAsync (#1113)
    Co-authored-by: rongzhimai <rongzhimai@whale.im>

--- openrlhf/trainer/ppo_trainer_async.py
+++ openrlhf/trainer/ppo_trainer_async.py
@@ -266,7 +266,7 @@ class PPOTrainerAsync:
         if self.args.remote_rm_url and not self.args.remote_rm_url[0] == "agent":
             from openrlhf.utils.remote_rm_utils import RemoteRewardModel
 
-            self.remote_reward_model = RemoteRewardModel.remote(self.args, self.remote_rm_url)
+            self.remote_reward_model = RemoteRewardModel.remote(self.args, self.args.remote_rm_url)
         else:
             self.remote_reward_model = None
 
