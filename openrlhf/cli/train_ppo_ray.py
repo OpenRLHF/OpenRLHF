917a8249352cda0e6f21a47573d41d646f37d48c
YiMing Liu <70597027+LYMDLUT@users.noreply.github.com>
2025-12-13 22:59:10 +0800

    Update train_ppo_ray.py

--- openrlhf/cli/train_ppo_ray.py
+++ openrlhf/cli/train_ppo_ray.py
@@ -342,23 +342,23 @@ if __name__ == "__main__":
     parser.add_argument("--max_samples", type=int, default=1e8, help="Max number of samples")
     parser.add_argument("--max_norm", type=float, default=1.0, help="Gradient clipping")
     parser.add_argument(
-        "--dist_backend", type=str, default="deepspeed", choices=["deepspeed", "fsdp"], help="Distributed backend"
+        "--dist_backend", type=str, default="deepspeed", choices=["deepspeed", "fsdp2"], help="Distributed backend"
     )
     parser.add_argument(
-        "--fsdp_offload",
+        "--fsdp2_offload",
         type=str,
         default="none",
         choices=["none", "cpu"],
         help="Fully-Sharded Data Parallel offload policy",
     )
     parser.add_argument(
-        "--fsdp_cpu_offload_pin_memory",
+        "--fsdp2_cpu_offload_pin_memory",
         action=argparse.BooleanOptionalAction,
         default=True,
-        help="Pin CPU memory when fsdp_offload=cpu",
+        help="Pin CPU memory when fsdp2_offload=cpu",
     )
     parser.add_argument(
-        "--fsdp_reshard_after_forward",
+        "--fsdp2_reshard_after_forward",
         action=argparse.BooleanOptionalAction,
         default=True,
         help="Control fully_shard reshard_after_forward flag",
