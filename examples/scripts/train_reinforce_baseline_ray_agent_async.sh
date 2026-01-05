dff1a2119b174766e5d9ddc5b4eb57ceac2da95b
xiaoxigua999 <xiaoxigua999@icloud.com>
2026-01-05 05:36:38 +0000

    fix rf++ example

--- examples/scripts/train_reinforce_baseline_ray_agent_async.sh
+++ examples/scripts/train_reinforce_baseline_ray_agent_async.sh
@@ -1,51 +1,108 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$0")"
+WORK_DIR=$(cd "$SCRIPT_DIR/../.." && pwd)
+
 set -x
 
-# reinforce++-baseline
+MODEL_PATH="Qwen/Qwen3-4B-Thinking-2507"
+DATASET_PATH="zhuzilin/dapo-math-17k"
+SAVE_PATH="${WORK_DIR}/exp/Qwen3-4B-Thinking"
+
+# For test
+AGENT_FUNC_PATH="examples/python/agent_func.py"
+
+# For demo
+# git clone https://github.com/Freder-chen/OpenRLHF-Agent.git
+# cd OpenRLHF-Agent && pip install -e .
+# AGENT_FUNC_PATH="{OpenRLHF-Agent/examples/single_turn/agent_func.py}"
+
+CKPT_ARGS=(
+   --pretrain ${MODEL_PATH}
+   # --reward_pretrain ${REWARD_MODEL}
+   --load_checkpoint
+
+   --save_path ${SAVE_PATH}
+   --ckpt_path "${SAVE_PATH}/ckpt"
+   --save_hf_ckpt
+   --max_ckpt_num 3
+   --save_steps 10
+)
+
+ROLLOUT_ARGS=(
+   --agent_func_path ${AGENT_FUNC_PATH}
+   # --remote_rm_url ${REWARD_FUNC_FILENAME}
+
+   --prompt_data ${DATASET_PATH}
+   --input_key prompt
+   --label_key label
+   --prompt_max_len 10240
+   --generate_max_len 64000
+   --apply_chat_template
+   --packing_samples
+
+   --rollout_batch_size 128
+   --n_samples_per_prompt 8
+   --train_batch_size 1024
+   --dynamic_filtering
+   --dynamic_filtering_reward_range 0.0 1.0
+
+   --use_dynamic_batch
+   --train_max_tokens_per_gpu 16192
+   --rollout_max_tokens_per_gpu 32768
+
+   --micro_train_batch_size 1
+   --micro_rollout_batch_size 8
+   --max_samples 128000
+   --max_epochs 1
+   --num_episodes 1
+)
+
+ENGINE_ARGS=(
+   --async_train
+
+   --ref_num_nodes 1
+   --ref_num_gpus_per_node 4
+   --actor_num_nodes 1
+   --actor_num_gpus_per_node 4
+   --vllm_num_engines 2
+   --vllm_tensor_parallel_size 2
+   --vllm_gpu_memory_utilization 0.7
+   --colocate_all_models
+   --deepspeed_enable_sleep
+   --vllm_sync_backend nccl
+   --enforce_eager
+
+   --zero_stage 3
+   --gradient_checkpointing
+   # --adam_offload
+   --ring_attn_size 2
+   --ring_head_stride 2
+   --bf16
+)
+
+OPTIMIZER_ARGS=(
+   --advantage_estimator reinforce_baseline
+   --actor_learning_rate 5e-7
+   # --critic_learning_rate 9e-6
+   --entropy_loss_coef 0.0
+   --init_kl_coef 1e-5
+   --use_kl_loss
+   --kl_estimator k2
+   --enable_vllm_is_correction
+   --use_icepop
+)
+
+LOG_ARGS=(
+   --use_tensorboard ${SAVE_PATH}/runs
+   --logging_steps 1
+   --eval_steps -1
+)
 
 ray job submit --address="http://127.0.0.1:8265" \
-   --runtime-env-json='{"working_dir": "/openrlhf"}' \
    -- python3 -m openrlhf.cli.train_ppo_ray \
-   --ref_num_nodes 1 \
-   --ref_num_gpus_per_node 6 \
-   --actor_num_nodes 1 \
-   --actor_num_gpus_per_node 6 \
-   --vllm_num_engines 2 \
-   --vllm_tensor_parallel_size 1 \
-   --colocate_all_models \
-   --pretrain OpenRLHF/Llama-3-8b-sft-mixture \
-   --agent_func_path /openrlhf/examples/python/agent_func.py \
-   --save_path /openrlhf/examples/test_scripts/checkpoint/llama3-8b-rlhf \
-   --micro_train_batch_size 8 \
-   --train_batch_size 192 \
-   --micro_rollout_batch_size 16 \
-   --rollout_batch_size 192 \
-   --n_samples_per_prompt 8 \
-   --max_epochs 1 \
-   --prompt_max_len 1024 \
-   --max_samples 12500 \
-   --generate_max_len 1024 \
-   --advantage_estimator reinforce_baseline \
-   --zero_stage 3 \
-   --bf16 \
-   --actor_learning_rate 5e-7 \
-   --init_kl_coef 1e-3 \
-   --use_kl_loss \
-   --kl_estimator k2 \
-   --prompt_data OpenRLHF/prompt-collection-v0.1 \
-   --input_key context_messages \
-   --apply_chat_template \
-   --normalize_reward \
-   --gradient_checkpointing \
-   --packing_samples \
-   --save_steps -1 \
-   --async_train \
-   --enable_vllm_is_correction \
-   --ckpt_path /openrlhf/examples/test_scripts/ckpt/llama3-8b-rlhf
-
-# --colocate_all_models with --async_train only merge the deepspeed models, not the vllm engines
-
-# You could also try
-#   --use_kl_loss \
-#   --kl_estimator k3 | k2 \
-
-# also supports --advantage_estimator rloo | reinforce_baseline
+   ${CKPT_ARGS[@]} \
+   ${ROLLOUT_ARGS[@]} \
+   ${ENGINE_ARGS[@]} \
+   ${OPTIMIZER_ARGS[@]} \
+   ${LOG_ARGS[@]}
